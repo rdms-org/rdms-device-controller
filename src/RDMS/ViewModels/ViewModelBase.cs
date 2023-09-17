c675edf95a2c3dddef4b2374213b55772a1b3023
June <junimiso04@naver.com>
2023-09-17 17:38:00 +0900

    feat: impl page navigation system

---
+++ src/RDMS/ViewModels/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RDMS.Behaviors.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace RDMS.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject, INavigationAware
+    {
+        private string _title = string.Empty;
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _description = string.Empty;
+
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+        public virtual void OnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/RDMS/Views/ShellWindow.xaml.cs
+++ src/RDMS/Views/ShellWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RDMS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace RDMS.Views
         public ShellWindow()
         {
             InitializeComponent();
+            DataContext = App.Current.Services?.GetService(typeof(ShellViewModel));
         }
     }
 }
