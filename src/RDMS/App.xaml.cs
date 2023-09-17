232330076b432a5e04111988676d35c5201c61aa
June <junimiso04@naver.com>
2023-09-17 19:46:30 +0900

    feat: add logging system and localization system

--- src/RDMS/App.xaml.cs
+++ src/RDMS/App.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using RDMS.Helpers;
 using RDMS.ViewModels;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Xaml;
@@ -31,8 +36,54 @@ namespace RDMS
         /// </summary>
         public App()
         {
+            ConfigureLocalization();
+            ConfigureLogging();
             ConfigureServices();
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Configures localization settings.
+        /// </summary>
+        private void ConfigureLocalization()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+
+            switch (Thread.CurrentThread.CurrentCulture.Name)
+            {
+                case "en-US":
+                    dict.Source = new Uri(@".\Assets\Localizations\Localization.en-US.xaml", UriKind.Relative);
+                    break;
+                case "ko-KR":
+                    dict.Source = new Uri(@".\Assets\Localizations\Localization.ko-KR.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri(@".\Assets\Localizations\Localization.en-US.xaml", UriKind.Relative);
+                    break;
+            }
+
+            App.Current.Resources.MergedDictionaries.Add(dict);
+        }
+
+        /// <summary>
+        /// Configures the logging system.
+        /// </summary>
+        private void ConfigureLogging()
+        {
+            string fileName = Path.Combine(VariableBuilder.GetBaseDirectory(), @"logs\.log");
+            string outputTemplateString = "{Timestamp:HH:mm:ss.ms} [{Level:u4}] {Message}{NewLine}{Exception}";
+
+            var log = new LoggerConfiguration()
+                .WriteTo.Async(a => a.File(fileName, restrictedToMinimumLevel: LogEventLevel.Verbose, shared: true, outputTemplate: outputTemplateString, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 10485760))
+                .CreateLogger();
+
+            Log.Logger = log;
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                Log.Fatal(e.ExceptionObject as Exception, "An unhandled exception has been occurred. If the same problem persists, please report it to the program provider.");
+                Log.CloseAndFlush();
+            };
         }
 
         /// <summary>
