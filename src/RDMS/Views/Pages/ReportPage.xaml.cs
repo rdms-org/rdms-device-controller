a5611e715d3732cd7140fafb4cb2f8d95e68875f
June <junimiso04@naver.com>
2023-09-23 20:38:34 +0900

    feat: add viewmodels to report and settings

--- src/RDMS/Views/Pages/ReportPage.xaml.cs
+++ src/RDMS/Views/Pages/ReportPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RDMS.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace RDMS.Views.Pages
         public ReportPage()
         {
             InitializeComponent();
+            DataContext = App.Current.Services?.GetService(typeof(ReportViewModel));
         }
     }
 }
