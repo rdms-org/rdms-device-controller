299ac363d7a82bb60e8733684707869be097a024
June <junimiso04@naver.com>
2023-10-03 01:31:26 +0900

    feat: design schedule page ui

--- src/RDMS/ViewModels/Pages/ScheduleViewModel.cs
+++ src/RDMS/ViewModels/Pages/ScheduleViewModel.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using RDMS.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace RDMS.ViewModels.Pages
 {
     public class ScheduleViewModel : ViewModelBase
     {
+        public XmlLanguage? LanguageResources { get; init; } = null;
+
         public ScheduleViewModel()
         {
             Title = "Schedule Page";
             Description = "The schedule viewer page";
+
+            // Initialize language resources.
+            LanguageResources = XmlLanguage.GetLanguage(LocalizationHelper.GetText("IDENTIFIER"));
         }
     }
 }
